bdc7493f9407d3449d58968852e63eece4ba3656
Tommy030 <tommysett2@gmail.com>
2021-03-08 09:55:02 +0100

    new

---
+++ assasination/Assets/Scripts/AI/EnemyMovement.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("Navmesh")]
+    public NavMeshHit Navhit;
+    public NavMeshPath m_navmeshpath;
+
+    private NavMeshAgent m_navmeshagent;
+    [SerializeField] public ScriptableEnemy m_Enemy;
+    private ScriptableEnemy m_PublicEnemy { set; get; }
+    private bool m_Alert;
+
+    private bool m_Wandering;
+    private bool m_newwait;
+    private Vector3 m_WanderLocation;
+    private LayerMask Layer;
+    [SerializeField] public float Angle = 10f;
+    [SerializeField] public float Radius;
+    private void Awake()
+    {
+        m_PublicEnemy = Instantiate(m_Enemy);
+
+        m_navmeshagent = gameObject.AddComponent<NavMeshAgent>();
+        m_navmeshagent.speed = m_PublicEnemy.m_Speed;
+        m_navmeshagent.acceleration = m_PublicEnemy.m_AccelerationSpeed;
+
+        gameObject.name = m_PublicEnemy.name;
+
+    }
+    public void AlertUnit(Vector3 targetpos)
+    {
+        if (m_navmeshagent.CalculatePath(targetpos, m_navmeshpath))
+        {
+            m_navmeshagent.SetPath(m_navmeshpath);
+            m_Alert = true;
+        }
+    }
+    private void Update()
+    {
+        if (m_Alert)
+        {
+            if (Vector3.Distance(gameObject.transform.position, m_navmeshagent.destination) < 0.3)
+            {
+
+            }
+        }
+        // else if (Vector3.Distance(Player.transform, transform.position)<= Radius)
+        // {
+        //   bool InFOXLOC = InFOV(transform, Player, Angle, Radius);
+        // }
+        else
+        {
+            if (!m_newwait)
+            {
+                Wander();
+            }
+        }
+    }
+
+    private void Wander()
+    {
+        if (!m_Wandering)
+        {
+            bool Haspath = false;
+
+            while (!Haspath && !m_Alert)
+            {
+                Haspath = RandomWanderTarget(transform.position, m_PublicEnemy.m_WanderRange, out m_WanderLocation);
+
+            }
+            if (Haspath && !m_Alert)
+            {
+                m_navmeshagent.SetDestination(m_WanderLocation);
+                m_Wandering = true;
+            }
+        }
+        if (m_navmeshagent.pathStatus == NavMeshPathStatus.PathComplete && m_Wandering)
+        {
+
+
+            StartCoroutine(WaitHere(m_Wandering, m_PublicEnemy.m_DurationWaitingAtWanderingLocation));
+        }
+    }
+
+    IEnumerator WaitHere(bool Change, float duration)
+    {
+        m_newwait = true;
+        m_Wandering = false;
+        yield return new WaitForSeconds(duration);
+        Wander();
+        m_newwait = false;
+    }
+    private bool RandomWanderTarget(Vector3 Centre, float range, out Vector3 Result)
+    {
+        Vector3 Randompoint = Centre + Random.insideUnitSphere * range;
+        if (NavMesh.SamplePosition(Randompoint, out Navhit, 1.0f, NavMesh.AllAreas))
+        {
+            Result = Navhit.position;
+            return true;
+        }
+        else
+        {
+            Result = Centre;
+            return false;
+        }
+    }
+
+    public bool InFOV(Transform Check, Transform Target, float MaxAngle, float Radius)
+    {
+        // adds colliders into this list
+        Collider[] Overlaps = new Collider[300];
+
+        //takes count of the amount of colliders in the radius drawn by the gizmos
+        int count = Physics.OverlapSphereNonAlloc(Check.position, Radius, Overlaps, Layer);
+
+        for (int i = 0; i < count + 1; i++)
+        {
+
+            if (Overlaps[i] != null)
+            {
+               
+                if (Overlaps[i].transform == Target)
+                {
+
+                    Vector3 directionbetween = (Target.position - Check.position).normalized;
+                    directionbetween.y = 0;
+
+                    float angle = Vector3.Angle(Check.forward, directionbetween);
+
+
+                    if (angle <= MaxAngle)
+                    {
+                       
+                        Ray ray = new Ray(Check.position, Target.position - Check.position);
+                        RaycastHit hit;
+                        if (Physics.Raycast(ray, out hit, Radius, Layer))
+                        {
+
+                            if (hit.transform == Target)
+                            {
+
+                                return true;
+                            }
+
+                        }
+
+                    }
+                    else
+                        return false;
+                }
+            }
+        }
+        return false;
+    }
+    private void OnDrawGizmos()
+    {
+        
+            Vector3 FovLine1 = Quaternion.AngleAxis(Angle, transform.up) * transform.forward * Radius;
+            Vector3 FovLine2 = Quaternion.AngleAxis(-Angle, transform.up) * transform.forward * Radius;
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, FovLine1);
+            Gizmos.DrawRay(transform.position, FovLine2);
+            Gizmos.color = Color.black;
+            Gizmos.DrawRay(transform.position, transform.forward * Radius);
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ assasination/Assets/Scripts/AI/ScriptableEnemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[CreateAssetMenu(fileName = "Enemy", menuName ="Create new Enemy")]
+public class ScriptableEnemy : ScriptableObject
+{
+    public string EnemyName = "Default";
+    [Header("Movement")]
+    public float m_Speed = 3;    
+    public float m_AccelerationSpeed = 1;
+
+    public float m_WanderRange = 10;
+    public float m_DurationWaitingAtWanderingLocation = 5;
+
+    [Header("Damage")]
+    public float m_Damage = 1;
+    
+}
