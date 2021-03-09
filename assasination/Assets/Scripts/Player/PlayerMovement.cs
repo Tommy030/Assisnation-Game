1b9138701c8ef7b6bffac2ce52f51992cbe98b22
wrxth <60602762+wrxth@users.noreply.github.com>
2021-03-09 11:26:05 +0100

    first person controller v.1
    de eerste versie van de controller

---
+++ assasination/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("references")]
+    [SerializeField] private CharacterController m_Control;
+    private Vector3 m_Velocity;
+
+    [Header("movement variables")]
+    [SerializeField] private float m_Movespeed;
+    [SerializeField] private float m_Gravity = 9.81f;
+    [SerializeField] private float m_JumpHeight;
+
+    [Header("Groundcheck")]
+    [SerializeField] private Transform m_GroundCheck;
+    [SerializeField] private float m_GroundDistance;
+    [SerializeField] private LayerMask m_GroundMask;
+    [SerializeField] private bool m_IsGrounded;
+
+
+    [Header("Crouch")]
+    [SerializeField] private Vector3 CurrentHeight;
+
+    private void Start()
+    {
+        CurrentHeight = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_IsGrounded = Physics.CheckSphere(m_GroundCheck.position, m_GroundDistance, m_GroundMask);
+
+        if (m_IsGrounded && m_Velocity.y < 0)
+        {
+            m_Velocity.y = -2;
+        }
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = transform.right * horizontal + transform.forward * vertical;
+
+        m_Control.Move(direction * m_Movespeed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && m_IsGrounded)
+        {
+            m_Velocity.y = Mathf.Sqrt(m_JumpHeight * -2f * m_Gravity);
+            Debug.Log("ping");
+        }
+
+        m_Velocity.y += -m_Gravity * Time.deltaTime;
+
+        m_Control.Move(m_Velocity * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            StartCrouch();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            StopCrouch();
+        }
+
+        transform.localScale = CurrentHeight;
+    }
+
+    private void StartCrouch()
+    {
+        CurrentHeight.y = 0.5f;
+    } 
+    private void StopCrouch()
+    {
+        CurrentHeight.y = 1f;
+    }
+}
