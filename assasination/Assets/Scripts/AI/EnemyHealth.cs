b757dc667fa8f51daaaa2c2cf0b1f4de5e42a171
wrxth <60602762+wrxth@users.noreply.github.com>
2021-03-14 17:22:28 +0100

    minor update

--- assasination/Assets/Scripts/AI/EnemyHealth.cs
+++ assasination/Assets/Scripts/AI/EnemyHealth.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EnemyHealth : MonoBehaviour
 {
     public ScriptableEnemy m_PublicEnemy;
+
+    private Target t;
+    private enemyState es;
+
+    private float m_CurrentHealth;
+    private void Start()
+    {
+        t = gameObject.GetComponent<Target>();
+        es = gameObject.GetComponent<enemyState>();
+        m_CurrentHealth = m_PublicEnemy.Health;
+    }
     public void RemoveHP(int damage)
     {
-        if (m_PublicEnemy.Health - damage > 0)
+        Debug.Log("hit");
+        es.hunting = true;
+        if (m_CurrentHealth - damage > 0)
         {
-            m_PublicEnemy.Health -= damage;
+            m_CurrentHealth -= damage;
         }
         else Death();
     }
     public void Death()
     {
+        if (t != null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 0);
+        }
         gameObject.SetActive(false);
     }
 }
