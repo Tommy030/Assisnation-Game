6a14e9816d3668da386c5b7709db850ca78af872
Tommy030 <tommysett2@gmail.com>
2021-03-22 09:56:33 +0100

    Main Menu + Build Index

---
+++ assasination/Assets/Scripts/UI/MainMenuButtons.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuButtons : MonoBehaviour
+{
+    public static MainMenuButtons m_instance;
+    
+    private void Awake()
+    {
+        if (m_instance == null) m_instance = this;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
+    }
+}
