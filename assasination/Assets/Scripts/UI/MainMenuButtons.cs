fc9a7579798369455fdd2132b1fd57ab7346c7af
Tommy030 <tommysett2@gmail.com>
2021-03-25 14:38:17 +0100

    was

--- assasination/Assets/Scripts/UI/MainMenuButtons.cs
+++ assasination/Assets/Scripts/UI/MainMenuButtons.cs
@@ -15,4 +15,8 @@ public class MainMenuButtons : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   
     }
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
 }
